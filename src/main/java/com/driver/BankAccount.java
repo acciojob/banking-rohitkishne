33400bb538c2454bdc9b0ab5b93931b88ed3382e
rohitkishne <rohitkishne3@gmail.com>
2023-03-24 19:16:02 +0530

    Banking Assignment Done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,75 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
-
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(sum>9*digits)
+        {
+            throw new Exception("Account Number can not be generated");
+        }
+        String accountNo = "";
+        while(sum>9)
+        {
+            accountNo = accountNo + '9';
+            sum -= 9;
+        }
 
-        return null;
+        accountNo = accountNo + sum;
+        while(accountNo.length()<digits)
+        {
+            accountNo = accountNo + '0';
+        }
+
+        return accountNo;
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance = balance + amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        double remBal = balance - amount; //remaining balance;
+        if(amount>balance || remBal < minBalance)
+        {
+            throw new Exception("Insufficient Balance");
+        }
+        else
+        {
+            balance = remBal;
+        }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -2,10 +2,15 @@ package com.driver;
 
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
-
+    private int mfc[] = new int[26];
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name,balance,5000.0);
+        this.tradeLicenseId = tradeLicenseId;
+        if(getMinBalance()<5000.0)
+        {
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -14,6 +19,109 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+        int n = tradeLicenseId.length();
+        if(!isValidId(tradeLicenseId))
+        {
+            int mfch = getMfc(tradeLicenseId);
+
+            if(n%2 == 0)
+            {
+                if(mfch >= (n/2+1)) {
+                    throw new Exception("Valid License can not be generated");
+                }
+            }
+            else
+            {
+                if(mfch >= (n/2+2))
+                {
+                    throw new Exception("Valid License can not be generated");
+                }
+            }
+            char maxFreqChar = getMaxfc(mfch);
+            char[] ans = new char[n];
+            int idx = 0;
+            for(idx = 0; idx<n; idx+=2)
+            {
+                if(mfc[maxFreqChar-'A']>0)
+                {
+                    ans[idx] = maxFreqChar;
+                    mfc[maxFreqChar-'A']--;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            for(int i=0; i<26; i++)
+            {
+                if(idx>=n)
+                {
+                    idx = 1;
+                }
+
+                while(mfc[i]>0)
+                {
+                    ans[idx] = (char)('A'+i);
+                    idx+=2;
+                }
+            }
+
+            String str = "";
+            for(int i=0; i<n; i++)
+            {
+                str = str + ans[i];
+            }
+
+            tradeLicenseId = str;
+        }
+
+    }
+
+    public Character getMaxfc(int max){
+        char ans = '0';
+        int maxFreq = max;
+        for(int i=0; i<26; i++)
+        {
+            if(mfc[i] == max)
+            {
+                ans = (char)('A' + i);
+                break;
+            }
+        }
+        return ans;
+    }
+    public int getMfc(String id)
+    {
+        int n = id.length();
+        for(int i=0; i<n; i++)
+        {
+            char ch = id.charAt(i);
+            mfc[ch-'A']++;
+        }
+        int max = -1;
+        for(int i=0; i<26; i++)
+        {
+            if(mfc[i]>max)
+            {
+                max = mfc[i];
+            }
+        }
+        return max;
+    }
+
+    public boolean isValidId(String id)
+    {
+        int n = id.length();
+        for(int i=0; i<n-1; i++)
+        {
+            char ch1 = id.charAt(i);
+            char ch2 = id.charAt(i+1);
+            if(ch1 == ch2)
+                return false;
+        }
+        return true;
     }
 
 }

815605141c4aa291ed7fdd2fe53cfee754b9ad9a
rohitkishne <rohitkishne3@gmail.com>
2023-03-30 14:18:46 +0530

    modification

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -25,10 +25,6 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
-        // If the characters of the license Id can be rearranged to create any valid license Id
-        // If it is not possible, throw "Valid License can not be generated" Exception
 
         int n = tradeLicenseId.length();
         int temp = 0;
